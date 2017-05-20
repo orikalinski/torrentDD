23a8fb937595d6bb6fc7b3fa9377b8c44be9d455
Ori Kalinski <ori@mintigo.com>
2017-05-20 17:45:27 +0300

    lower issue

--- torrentDD/download_via_torrent.py
+++ torrentDD/download_via_torrent.py
@@ -158,8 +158,8 @@ class SubtitlesDownloader(BaseDownloader):
         download_id = None
         final_download_version = None
         for button_text, version in zip(buttons_text, versions):
-            version = re.search(VERSION_REGEX_PATTERN % episode, version.text.lower(), re.I).group(1)
-            if not download_id or Levenshtein.ratio(version, download_version.lower()) > SIMILARITY_THRESHOLD:
+            version = re.search(VERSION_REGEX_PATTERN % episode, version.text, re.I).group(1)
+            if not download_id or Levenshtein.ratio(version.lower(), download_version.lower()) > SIMILARITY_THRESHOLD:
                 final_download_version = version
                 download_id = DOWNLOAD_REGEX.search(button_text.find("a").get("onclick")).group(1)
         if download_id:
